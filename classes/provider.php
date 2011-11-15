7a424d2c2b29db6e071117b3e3ecd367bded1381
Phil Sturgeon <email@philsturgeon.co.uk>
2011-11-15 12:45:46 +0000

    Added the default scope separator similar to the oath package.

--- classes/provider.php
+++ classes/provider.php
@@ -45,6 +45,11 @@ abstract class Provider {
 	protected $params = array();
 	
 	/**
+	 * @var  string  scope separator, most use "," but some like Google are spaces
+	 */
+	public $scope_seperator = ',';
+	
+	/**
 	 * @var  string  the method to use when requesting tokens
 	 */
 	protected $method = 'GET';
@@ -115,16 +120,14 @@ abstract class Provider {
 	{
 		$state = md5(uniqid(rand(), TRUE));
 		\Session::set('state', $state);
-			
-		$params = array(
-			'client_id' => $this->client_id,
-			'redirect_uri' => \Arr::get($options, 'redirect_uri', $this->redirect_uri),
-			'state' => $state,
-			'response_type' => 'code',
-			'scope' => $this->scope,
-		);
 		
-		$url = $this->url_authorize().'?'.http_build_query($params);
+		$url = $this->url_authorize().'?'.http_build_query(array(
+			'client_id' 		=> $this->client_id,
+			'redirect_uri' 		=> \Arr::get($options, 'redirect_uri', $this->redirect_uri),
+			'state' 			=> $state,
+			'scope'     		=> is_array($this->scope) ? implode($this->scope_seperator, $this->scope) : $this->scope,
+			'response_type' 	=> 'code',
+		));
 		
 		\Response::redirect($url);
 	}
@@ -138,11 +141,11 @@ abstract class Provider {
 	public function access($code, $options = array())
 	{
 		$params = array(
-			'client_id' => $this->client_id,
+			'client_id' 	=> $this->client_id,
 			'client_secret' => $this->client_secret,
-			'redirect_uri' => \Arr::get($options, 'redirect_uri', $this->redirect_uri),
-			'code' => $code,
-			'grant_type' => 'authorization_code'
+			'redirect_uri' 	=> \Arr::get($options, 'redirect_uri', $this->redirect_uri),
+			'code' 			=> $code,
+			'grant_type' 	=> 'authorization_code'
 		);
 	
 		$response = null;	
@@ -156,6 +159,7 @@ abstract class Provider {
 				
 				parse_str($response, $params); 
 				break;
+				
 			case 'POST':
 				//maybe switch to use curl?
 				/*
@@ -166,8 +170,8 @@ abstract class Provider {
 				$response = $curl->post('', $params);*/
 				
 				$postdata = http_build_query($params);
-				$opts = array('http' =>
-					array(
+				$opts = array(
+					'http' => array(
 						'method'  => 'POST',
 						'header'  => 'Content-type: application/x-www-form-urlencoded',
 						'content' => $postdata
