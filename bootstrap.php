ea9e72adcf68144d9a04e2cbb722728b367440dc
crynobone <crynobone@gmail.com>
2011-12-15 15:22:43 +0800

    Add Foursquare provider

--- bootstrap.php
+++ bootstrap.php
@@ -17,6 +17,7 @@ Autoloader::add_classes(array(
 
 	'OAuth2\\Provider'  			=> __DIR__.'/classes/provider.php',
 	'OAuth2\\Provider_Facebook'  	=> __DIR__.'/classes/provider/facebook.php',
+	'OAuth2\\Provider_Foursquare'  	=> __DIR__.'/classes/provider/foursquare.php',
 	'OAuth2\\Provider_Google'  		=> __DIR__.'/classes/provider/google.php',
 	'OAuth2\\Provider_Github'  		=> __DIR__.'/classes/provider/github.php',
 	'OAuth2\\Provider_Unmagnify'  	=> __DIR__.'/classes/provider/unmagnify.php',
